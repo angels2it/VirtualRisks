a10edfac999a0ce082dff17c5f68c44a0798f04d
AiT <cuong.pham@eton.vn>
2017-12-28 13:28:00 +0700

    add castleview

---
+++ Mobiles/VirtualRisks.Mobiles/VirtualRisks.Mobiles.Android/Views/CastleView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Droid.Support.Design;
+using MvvmCross.Droid.Views.Attributes;
+
+namespace VirtualRisks.Mobiles.Droid.Views
+{
+    [MvxDialogFragmentPresentation]
+    public class CastleView : MvxBottomSheetDialogFragment
+    {
+        public CastleView()
+        {
+        }
+
+        protected CastleView(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            Cancelable = false;
+            var ignore = base.OnCreateView(inflater, container, savedInstanceState);
+            var view = this.BindingInflate(Resource.Layout.LoginView, null);
+            return view;
+        }
+    }
+}
